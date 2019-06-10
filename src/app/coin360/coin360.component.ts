93fcb0f4bef746981ea90f81bf2ffb0ec32243da
Ignacio Álvarez Arenas <ignacio_aaa@hotmail.com>
2019-06-09 23:21:43 -0400

    update 360: se testea para thumb en phone

--- src/app/coin360/coin360.component.ts
+++ src/app/coin360/coin360.component.ts
@@ -20,21 +20,6 @@ export class Coin360Component implements OnInit {
                 height: '900px',
                 thumbnailsColumns: 3,
                 imageAnimation: NgxGalleryAnimation.Slide
-            },
-            // max-width 800
-            {
-                breakpoint: 800,
-                width: '100%',
-                height: '600px',
-                imagePercent: 80,
-                thumbnailsPercent: 20,
-                thumbnailsMargin: 20,
-                thumbnailMargin: 20
-            },
-            // max-width 400
-            {
-                breakpoint: 400,
-                preview: false
             }
         ];
  
