c18b5d064506a570e2e24d464725a3a190f122bf
Ignacio Álvarez Arenas <ignacio_aaa@hotmail.com>
2019-06-09 23:29:43 -0400

    update: se replica 360 fix. Se incluyen css

--- src/app/coinmarketcap/coinmarketcap.component.ts
+++ src/app/coinmarketcap/coinmarketcap.component.ts
@@ -20,21 +20,6 @@ export class CoinmarketcapComponent implements OnInit {
                 height: '900px',
                 thumbnailsColumns: 2,
                 imageAnimation: NgxGalleryAnimation.Slide
-            },
-            // max-width 800
-            {
-                breakpoint: 800,
-                width: '100%',
-                height: '600px',
-                imagePercent: 80,
-                thumbnailsPercent: 20,
-                thumbnailsMargin: 20,
-                thumbnailMargin: 20
-            },
-            // max-width 400
-            {
-                breakpoint: 400,
-                preview: false
             }
         ];
  
